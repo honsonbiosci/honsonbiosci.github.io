34b348bfc9e9706f12e3317fb05e442e468e229b
Drew Honson <dhonson@lncrna.caltech.edu>
2023-09-07 09:34:10 -0700

    Logo

--- source/conf.py
+++ source/conf.py
@@ -51,6 +51,12 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 html_favicon = 'images/favicon.ico'
+html_logo = 'images/logo.png'
+
+html_theme_options = {
+    "logo_only": True,
+    "display_version": False,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
